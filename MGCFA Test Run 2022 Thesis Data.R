37bbd1b69ec3e048f826fd620cf5a871b31b8ecf
Jasmin Fernandez Castillo <63816241+jf2987@users.noreply.github.com>
2022-05-02 17:49:14 -0700

    Add files via upload

--- MGCFA Test Run 2022 Thesis Data.R
+++ MGCFA Test Run 2022 Thesis Data.R
@@ -468,7 +468,127 @@ summary(overall.fit,
         rsquare = TRUE, 
         fit.measure = TRUE)
 
+#### Create Table for Symbolic Racism Fit Statistics
+library(knitr)
+table_fit <- matrix(NA, nrow = 9, ncol = 6)
+colnames(table_fit) = c("Model", "X2", "df", "CFI", "RMSEA", "SRMR")
+table_fit[1, ] <- c("Overall Model", round(fitmeasures(overall.fit, 
+                                                       c("chisq", "df", "cfi",
+                                                         "rmsea", "srmr")),3))
+kable(table_fit)
+
+## Do a model specific per condition : BTW
+
+BTW.fit <- cfa(model = overall.model,
+               data = CleanData[CleanData$Condition=="BTW", ], 
+               meanstructure = TRUE) ##this is important 
+summary(BTW.fit, 
+        standardized = TRUE, 
+        rsquare = TRUE, 
+        fit.measure = TRUE)
+## Make table with the results of BTW
+
+table_fit[2, ] <- c("BTW Model", round(fitmeasures(BTW.fit, 
+                                                   c("chisq", "df", "cfi",
+                                                     "rmsea", "srmr")),3))
+kable(table_fit)
+
+
+## Do a model specific per condition : wm
+
+wm.fit <- cfa(model = overall.model,
+              data = CleanData[CleanData$Condition=="wm", ], 
+              meanstructure = TRUE) ##this is important 
+summary(wm.fit, 
+        standardized = TRUE, 
+        rsquare = TRUE, 
+        fit.measure = TRUE)
+## apparently only 330 of 333 rows were used for the white model
+
+table_fit[3, ] <- c("wm Model", round(fitmeasures(wm.fit, 
+                                                  c("chisq", "df", "cfi",
+                                                    "rmsea", "srmr")),3))
+kable(table_fit)
+
+
+## Configural Invariance
+
+configural.fit <- cfa(model = overall.model,
+                      data = CleanData, 
+                      meanstructure = TRUE,
+                      group = "Condition") ##this is important 
+
+summary(configural.fit, 
+        standardized = TRUE, 
+        rsquare = TRUE, 
+        fit.measure = TRUE)
+
+table_fit[4, ] <- c("Configural Model", round(fitmeasures(configural.fit, 
+                                                          c("chisq", "df", "cfi",
+                                                            "rmsea", "srmr")),3))
+kable(table_fit)
+
+### MEtric Invariance
+
+metric.fit <- cfa(model = overall.model,
+                  data = CleanData, 
+                  meanstructure = TRUE,
+                  group = "Condition",
+                  group.equal=c("loadings")) ##this is important 
+
+summary(metric.fit, 
+        standardized = TRUE, 
+        rsquare = TRUE, 
+        fit.measure = TRUE)
+
+table_fit[5, ] <- c("Metric Model", round(fitmeasures(metric.fit, 
+                                                      c("chisq", "df", "cfi",
+                                                        "rmsea", "srmr")),3))
+kable(table_fit)
+
+## Scalar Invariance
+
+scalar.fit <- cfa(model = overall.model,
+                  data = CleanData, 
+                  meanstructure = TRUE,
+                  group = "Condition",
+                  group.equal=c("loadings", "intercepts")) ##this is important 
+
+summary(scalar.fit, 
+        standardized = TRUE, 
+        rsquare = TRUE, 
+        fit.measure = TRUE)
+
+table_fit[6, ] <- c("Scalar Model", round(fitmeasures(scalar.fit, 
+                                                      c("chisq", "df", "cfi",
+                                                        "rmsea", "srmr")),3))
+kable(table_fit)
+
+## Strict Invariance
+
+strict.fit <- cfa(model = overall.model,
+                  data = CleanData, 
+                  meanstructure = TRUE,
+                  group = "Condition",
+                  group.equal=c("loadings", "intercepts", "residuals")) ##this is important 
+
+
+summary(strict.fit, 
+        standardized = TRUE, 
+        rsquare = TRUE, 
+        fit.measure = TRUE)
+
+
+table_fit[7, ] <- c("Strict Model", round(fitmeasures(strict.fit, 
+                                                      c("chisq", "df", "cfi",
+                                                        "rmsea", "srmr")),3))
+kable(table_fit)
+
+
 ## Understanding Lavaan Degrees of freedom 
 ## https://stackoverflow.com/questions/52937569/understanding-degrees-of-freedom-in-lavaan
 ### Hierarchal CFA
 ## https://www.youtube.com/watch?v=C8U9hanos1U
+
+names(CleanData)
+overall.model = ' TransPH =~ SimRac1 + SimRac2 + SimRac3 + SimRac4 + SimRac5 + SimRac6 + SimRac7 + SimRac8'
