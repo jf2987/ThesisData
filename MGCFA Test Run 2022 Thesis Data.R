1fc195a536030dcd7701bd97137f51d84d5c3377
Jasmin Fernandez Castillo <63816241+jf2987@users.noreply.github.com>
2022-05-02 18:20:37 -0700

    Add files via upload

--- MGCFA Test Run 2022 Thesis Data.R
+++ MGCFA Test Run 2022 Thesis Data.R
@@ -457,7 +457,7 @@ dim(CleanData)
 ## goes from 678 to 667
 
 names(CleanData)
-overall.model = ' TransPH =~ SimRac1 + SimRac2 + SimRac3 + SimRac4 + SimRac5 + SimRac6 + SimRac7 + SimRac8'
+overall.model = ' SimRac =~ SimRac1 + SimRac2 + SimRac3 + SimRac4 + SimRac5 + SimRac6 + SimRac7 + SimRac8'
 
 library(lavaan)
 overall.fit <- cfa(model = overall.model,
@@ -590,5 +590,164 @@ kable(table_fit)
 ### Hierarchal CFA
 ## https://www.youtube.com/watch?v=C8U9hanos1U
 
+
+setwd("C:/Users/cogps/Desktop/Thesis Analysis")
+CleanData <- read.csv("Clean_Data_2022_Latest.csv") 
+
 names(CleanData)
-overall.model = ' TransPH =~ SimRac1 + SimRac2 + SimRac3 + SimRac4 + SimRac5 + SimRac6 + SimRac7 + SimRac8'
+dim(CleanData)
+## Delete fully empty rows
+CleanData<-CleanData[!apply(is.na(CleanData[,6:37]), 1, all),]
+dim(CleanData)
+## goes from 678 to 669
+
+names(CleanData)
+overall.model = ' 
+SimRac =~ SimRac1 + SimRac2 + SimRac3 + SimRac4 + SimRac5 + SimRac6 + SimRac7 + SimRac8;
+SSIT =~ SepSpher1 + SepSpher2 + SepSpher3 + SepSpher4 + SepSpher5 + SepSpher6 + SepSpher7 + SepSpher8 + SepSpher9 + SepSpher10 + SepSpher11 + SepSpher12 + SepSpher13 + SepSpher14 + SepSpher15;
+TransPH =~ TransPH1 + TransPH2 + TransPH3 + TransPH4 + TransPH5 + TransPH6 + TransPH7 + TransPH8 + TransPH9
+global=~ SimRac + SSIT + TransPH
+'
+
+library(lavaan)
+overall.fit <- cfa(model = overall.model,
+                   data = CleanData, 
+                   meanstructure = TRUE) ##this is important 
+summary(overall.fit, 
+        standardized = TRUE, 
+        rsquare = TRUE, 
+        fit.measure = TRUE)
+
+#### Create Table for The hierarchal Model Fit Statistics
+library(knitr)
+table_fit <- matrix(NA, nrow = 9, ncol = 6)
+colnames(table_fit) = c("Model", "X2", "df", "CFI", "RMSEA", "SRMR")
+table_fit[1, ] <- c("Overall Model", round(fitmeasures(overall.fit, 
+                                                       c("chisq", "df", "cfi",
+                                                         "rmsea", "srmr")),3))
+kable(table_fit)
+
+## Do a model specific per condition : BTW
+
+BTW.fit <- cfa(model = overall.model,
+               data = CleanData[CleanData$Condition=="BTW", ], 
+               meanstructure = TRUE) ##this is important 
+summary(BTW.fit, 
+        standardized = TRUE, 
+        rsquare = TRUE, 
+        fit.measure = TRUE)
+
+## Make table with the results of BTW
+
+table_fit[2, ] <- c("BTW Model", round(fitmeasures(BTW.fit, 
+                                                   c("chisq", "df", "cfi",
+                                                     "rmsea", "srmr")),3))
+kable(table_fit)
+
+
+## Do a model specific per condition : wm
+
+wm.fit <- cfa(model = overall.model,
+              data = CleanData[CleanData$Condition=="wm", ], 
+              meanstructure = TRUE) ##this is important 
+summary(wm.fit, 
+        standardized = TRUE, 
+        rsquare = TRUE, 
+        fit.measure = TRUE)
+## apparently only 330 of 333 rows were used for the white model
+
+table_fit[3, ] <- c("wm Model", round(fitmeasures(wm.fit, 
+                                                  c("chisq", "df", "cfi",
+                                                    "rmsea", "srmr")),3))
+kable(table_fit)
+
+
+## Configural Invariance
+
+configural.fit <- cfa(model = overall.model,
+                      data = CleanData, 
+                      meanstructure = TRUE,
+                      group = "Condition") ##this is important 
+
+summary(configural.fit, 
+        standardized = TRUE, 
+        rsquare = TRUE, 
+        fit.measure = TRUE)
+
+table_fit[4, ] <- c("Configural Model", round(fitmeasures(configural.fit, 
+                                                          c("chisq", "df", "cfi",
+                                                            "rmsea", "srmr")),3))
+kable(table_fit)
+
+### MEtric Invariance
+
+metric.fit <- cfa(model = overall.model,
+                  data = CleanData, 
+                  meanstructure = TRUE,
+                  group = "Condition",
+                  group.equal=c("loadings")) ##this is important 
+
+summary(metric.fit, 
+        standardized = TRUE, 
+        rsquare = TRUE, 
+        fit.measure = TRUE)
+
+table_fit[5, ] <- c("Metric Model", round(fitmeasures(metric.fit, 
+                                                      c("chisq", "df", "cfi",
+                                                        "rmsea", "srmr")),3))
+kable(table_fit)
+
+## Scalar Invariance
+
+scalar.fit <- cfa(model = overall.model,
+                  data = CleanData, 
+                  meanstructure = TRUE,
+                  group = "Condition",
+                  group.equal=c("loadings", "intercepts")) ##this is important 
+## For Scalar fit we get a warning 
+
+# Warning message:
+#   In lav_model_vcov(lavmodel = lavmodel, lavsamplestats = lavsamplestats,  :
+#                       lavaan WARNING:
+#                       The variance-covariance matrix of the estimated parameters (vcov)
+#                     does not appear to be positive definite! The smallest eigenvalue
+#                     (= 2.218850e-15) is close to zero. This may be a symptom that the
+#                     model is not identified.
+summary(scalar.fit, 
+        standardized = TRUE, 
+        rsquare = TRUE, 
+        fit.measure = TRUE)
+
+table_fit[6, ] <- c("Scalar Model", round(fitmeasures(scalar.fit, 
+                                                      c("chisq", "df", "cfi",
+                                                        "rmsea", "srmr")),3))
+kable(table_fit)
+
+## Strict Invariance
+
+strict.fit <- cfa(model = overall.model,
+                  data = CleanData, 
+                  meanstructure = TRUE,
+                  group = "Condition",
+                  group.equal=c("loadings", "intercepts", "residuals")) ##this is important 
+## Another warning 
+# Warning message:
+#   In lav_model_vcov(lavmodel = lavmodel, lavsamplestats = lavsamplestats,  :
+#                       lavaan WARNING:
+#                       The variance-covariance matrix of the estimated parameters (vcov)
+#                     does not appear to be positive definite! The smallest eigenvalue
+#                     (= 1.374505e-15) is close to zero. This may be a symptom that the
+#                     model is not identified.
+
+
+summary(strict.fit, 
+        standardized = TRUE, 
+        rsquare = TRUE, 
+        fit.measure = TRUE)
+
+
+table_fit[7, ] <- c("Strict Model", round(fitmeasures(strict.fit, 
+                                                      c("chisq", "df", "cfi",
+                                                        "rmsea", "srmr")),3))
+kable(table_fit)
+
